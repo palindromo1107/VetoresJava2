60b7907ebf3afba9c9a6303a3715ed4749f707c6
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 15:01:52 -0300

    Initialize Main class with arrays A and B

---
+++ Questao5/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+        int[] A = {1,2,43,4,0};
+        int[] B = {1,62,4,0,9};
+        int[] C = new int[10];
+        
+        for (int i = 0; i < A.length; i++) {
+            
+        }
+	}
+}

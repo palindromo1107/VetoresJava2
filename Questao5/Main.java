b43f3ed2acd943020583da31066624c26295cce4
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 18:50:35 -0300

    Update Main.java

--- Questao5/Main.java
+++ Questao5/Main.java
@@ -1,13 +1,33 @@
-public class Main
-{
-	public static void main(String[] args) {
-	    
-        int[] A = {1,2,43,4,0};
-        int[] B = {1,62,4,0,9};
-        int[] C = new int[10];
-        
+public class Main {
+    public static void main(String[] args) {
+
+        int[] A = { 1, 2, 43, 4, 0, 1, 62, 4, 0, 9 };
+        int[] C = new int[A.length];
+        int tamFinal = 0;
+
         for (int i = 0; i < A.length; i++) {
-            
+            if (diferente(C, tamFinal, A[i])) {
+                C[tamFinal] = A[i];
+                tamFinal++;
+            }
         }
-	}
+
+        int[] B = new int[tamFinal];
+
+        for (int i = 0; i < B.length; i++) {
+            B[i] = C[i];
+        }
+
+    }
+
+    public static boolean diferente(int[] lista, int tamanho, int item) {
+
+        for (int i = 0; i < tamanho; i++) {
+            if (lista[i] == item) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 }

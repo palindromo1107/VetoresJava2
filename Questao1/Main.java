3cf5388b7464b989e45a08bc54ca0c35c173b1f0
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 14:21:02 -0300

    Implement integer input and output in Main.java
    Add a Java program to read and print 10 integers.

---
+++ Questao1/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner input = new Scanner(System.in);
+	    
+	    int[] A = new int[10];
+	    
+		A[0] = scanner.nextInt();
+		A[1] = scanner.nextInt();
+		A[2] = scanner.nextInt();
+		A[3] = scanner.nextInt();
+		A[4] = scanner.nextInt();
+		A[5] = scanner.nextInt();
+		A[6] = scanner.nextInt();
+		A[7] = scanner.nextInt();
+		A[8] = scanner.nextInt();
+		A[9] = scanner.nextInt();
+		
+		System.out.println("numeros lidos...");
+		
+		for (int i = 0; i < A.length; i++) {
+		    System.out.println(A[i]);
+		} 
+		
+	}
+}

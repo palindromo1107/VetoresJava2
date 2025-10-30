38920493256bbe645712824cf5ade5eaea740c8a
Mateus <mateusdesouzaroseno@gmail.com>
2025-10-30 19:24:29 -0300

    Create Main.java

---
+++ Questao9/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] A = { 42, 7, 99, 18, 63, 54, 27, 82, 100, 11 };
+
+        Arrays.sort(A);
+
+    }
+}
